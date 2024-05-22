035090e2905059d4701318faa7cd2c615ce38566
Reddy Kilowatt <cappy2112@gmail.com>
2024-05-21 23:48:17 -0700

    terminate_process() works- when running as an admin, and only tested on 3 processes. Need to think about killing each process after it finishes, so that if the user does a lot of ptaches (100s) the program doesn't have to wat until the end and kill all of them

--- editor_interface.py
+++ editor_interface.py
@@ -43,6 +43,22 @@ class EditorInterface:
         """
         status = 0
 
+        # this seems to work- gotta be an admin
+        print(f"\nTerminating {len(self.processes)} subprocesses\n")
+        for proc in self.processes:
+            print(f"Terminating subprocess ID: {proc.pid}")
+            # proc.kill()
+            proc.terminate() # it looks like the editor was terminated
+            proc.wait(25)
+            
+        return status
+
+    def terminate_process1(self):
+        """Provide a method to the caller to terminate the subprocess so as not to leave
+        any zombies running.
+        """
+        status = 0
+
         # gotta be an admin for this
         # result = sub.run(["taskkill", "/im", Path(self.editor_path).name, "/f", "/t"], timeout=10)
         term_proc = sub.Popen(["taskkill", "/im", Path(self.editor_path).name, "/f", "/t"])
