de8966d7d910e35116bb56ac90be476e686f3782
Reddy Kilowatt <cappy2112@gmail.com>
2024-05-24 12:45:06 -0700

    Modified _process_common() so that when called from start_editor(), and empty string is not passed to the editor as an argument, because it casues the editor to display a 'file not found' message

--- editor_interface.py
+++ editor_interface.py
@@ -20,26 +20,20 @@ class EditorInterface:
         sending patches, the editor will already be running an no notes will be missing/lsot due to the slow startup.
         """
         status = 0
-        try:
-            args = [self.editor_path]
-            self.processes.append(sub.Popen(args, universal_newlines=True, shell=False))
-        except FileNotFoundError:
-            status = 1
-            print(f"\nERROR: File Not Found: {self.editor_path}\n")
-        except sub.SubprocessError as exc:
-            status = 1
-            print(f'\nERROR occurred launching {self.editor_path}. Verify that the patch is correct.: {exc}\n\n')
         
-    def send_patch(self, absolute_path_to_patch):
-        """
-        Send a patch to the Nord editor 
+        status = self._process_common()
+        if status:
+            print(f'\nERROR occurred launching {self.editor_path}. Verify that the patch is correct.\n')
 
-        Args:
-             (str): A string containing the absolute path to a G1 patch file
-        """
+        
+    def _process_common(self, absolute_path_to_patch=''):
+
         status = 0
-        try:
+        args = [self.editor_path]
+        if absolute_path_to_patch:
             args = [self.editor_path, absolute_path_to_patch.strip()]
+
+        try:
             self.processes.append(sub.Popen(args, universal_newlines=True, shell=False))
             # self.editor.stdin.write(absolute_path_to_patch) # writing to stdin doesn't work
         except FileNotFoundError:
@@ -47,10 +41,21 @@ class EditorInterface:
             print(f"\nERROR: File Not Found: {self.editor_path}\n")
         except sub.SubprocessError as exc:
             status = 1
-            print(f'\nERROR occurred sending {Path(absolute_path_to_patch).name} to the Nord Editor: {exc}\n\n')
-
-
+            print(f'\n{exc}\n')
+            
         return status
+    
+    
+    def send_patch(self, absolute_path_to_patch):
+        """
+        Send a patch to the Nord editor 
+
+        Args:
+             (str): A string containing the absolute path to a G1 patch file
+        """
+        status = self._process_common(absolute_path_to_patch)
+        if status:
+            print(f'\nERROR occurred sending {Path(absolute_path_to_patch).name} to the Nord Editor:\n')
 
     def terminate_process(self):
         """Provide a method to the caller to terminate the subprocess so as not to leave
@@ -59,12 +64,20 @@ class EditorInterface:
         status = 0
 
         # FOR THIS TO WORK, THE USER ***MUST*** RUN THE PROGRAM AS ADMINISTRATOR
-        print(f"\nTerminating {len(self.processes)} subprocesses\n")
+        if self.processes:
+            print(f"\nTerminating {len(self.processes)} subprocesses\n")
+        else:
+            print('\nThe subprocesses for  the editor have already been terminated.\n')
+            
         for proc in self.processes:
             print(f"Terminating subprocess ID: {proc.pid}")
             # proc.kill()
-            proc.terminate() # it looks like the editor was terminated
-            proc.wait(25)
+            try:
+                proc.terminate() # terminate this process and wait, don't leave any zombies running
+                proc.wait(25)
+            except sub.SubprocessError as exc:
+                status = 1
+                print(f'\n{exc}\n\nException occurred terminating process ID: {proc.pid}. You should manually terminate this process, or reboot.\n')
             
         return status
 
